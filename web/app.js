c1642fab166bb6f45bbf4c51c16384eb7741d0c7
Harendra Singh <2011195@iiitdmj.ac.in>
2015-08-29 23:20:19 +0530

    applied bootstrap

--- web/app.js
+++ web/app.js
@@ -1,33 +1,30 @@
-﻿function callApi() {
-    $("#row1").addClass("loadingImg");
-    $("#row2").addClass("loadingImg");
-    $("#row3").addClass("loadingImg");
-    $("#row4").addClass("loadingImg");
-    date24HoursBefore = new Date(new Date().getTime() - (24 * 60 * 60 * 1000));
-    date7daysBefore = new Date(new Date().getTime() - (7 * 24 * 60 * 60 * 1000));
+﻿// Global variables holding the count of issues.
+total = 0, last24Hours = 0, last7Days = 0, after7Days = 0;
 
-    url = $("#urlInput").val().trim().
+// Github API sends limited no of results (100 max), so need to request in form of pages 
+pageNo = 1;
+
+//Checks the URL entered.
+function checkURL() {
+	url = $("#urlInput").val().trim().
         match("(http:\/\/|https:\/\/)?github\.com\/([a-zA-z_\\-0-9]+\/[a-zA-Z_\\-0-9]+)\/?");
     if (url && url[0] && $("#urlInput").val() == url[0]) {
-        $("#errorMsg").hide();
-
-        total = 0, last24Hours = 0, last7Days = 0, after7Days = 0;
-        pageNo = 1;
-        callPage();
+        $("#errorDiv").hide();
+        callAPI();
     }
     else {
-        $("#errorMsg").html("Invalid URL. Please Use format https://github.com/[user]/[repository]");
-        $("#errorMsg").show();
-        //setTimeout(function () { $("#errorMsg").hide(); }, 3000);
-        $("#row1").removeClass("loadingImg");
-        $("#row2").removeClass("loadingImg");
-        $("#row3").removeClass("loadingImg");
-        $("#row4").removeClass("loadingImg");
-        return;
+        $("#errorMesg").html("Invalid URL. Please Use format https://github.com/[user]/[repository]");
+        $("#errorDiv").show();
+        updateRows();
+        return false;
     }
 }
-function callPage() {
-    console.log("https://api.github.com/repos/" + url[2] + "/issues?state=all&per_page=100&page=" + pageNo);
+
+// Calls the Github API (api.github.com)
+function callAPI() {
+	date24HoursBefore = new Date(new Date().getTime() - (24 * 60 * 60 * 1000));
+    date7daysBefore = new Date(new Date().getTime() - (7 * 24 * 60 * 60 * 1000));
+    addLoadingGIF();
     $.get("https://api.github.com/repos/" + url[2] + "/issues?state=all&per_page=100&page=" + pageNo)
         .done(function (d) {
             console.log(d);
@@ -44,33 +41,43 @@ function callPage() {
                 }
             }
             if (d.length == 100) {
-                console.log(d.length);
                 pageNo++;
-                $("#row1").html(total);
-                $("#row2").html(last24Hours);
-                $("#row3").html(last7Days);
-                $("#row4").html(after7Days);
-                callPage();
+				updateRows(total,last24Hours,last7Days,after7Days);
+                callAPI();
             }
             else {
-                $("#row1").html(total);
-                $("#row2").html(last24Hours);
-                $("#row3").html(last7Days);
-                $("#row4").html(after7Days);
-
-                $("#row1").removeClass("loadingImg");
-                $("#row2").removeClass("loadingImg");
-                $("#row3").removeClass("loadingImg");
-                $("#row4").removeClass("loadingImg");
+				updateRows(total,last24Hours,last7Days,after7Days);
+                removeLoadingGIF();
             }
         })
 		.fail(function(e){
-			$("#errorMsg").html("Error : "+e.status+" : "+e.statusText);
-			$("#errorMsg").show();
-			$("#row1").removeClass("loadingImg");
-			$("#row2").removeClass("loadingImg");
-			$("#row3").removeClass("loadingImg");
-			$("#row4").removeClass("loadingImg");
+			$("#errorMesg").html("Error : "+e.status+" : "+e.statusText);
+			$("#errorDiv").show();
+			removeLoadingGIF();
 		});
 		
+}
+
+// Inserts the values in the Rows
+function updateRows(row1,row2,row3,row4){
+	$("#row1").html(row1?row1:"");
+	$("#row2").html(row2?row2:"");
+	$("#row3").html(row3?row3:"");
+	$("#row4").html(row4?row4:"");
+}
+
+// Removes the Loading GIF from the rows
+function removeLoadingGIF(){
+    $("#row1").removeClass("loadingImg");
+	$("#row2").removeClass("loadingImg");
+	$("#row3").removeClass("loadingImg");
+	$("#row4").removeClass("loadingImg");
+}
+
+// Adds the Loading GIF to the rows
+function addLoadingGIF(){
+    $("#row1").addClass("loadingImg");
+    $("#row2").addClass("loadingImg");
+    $("#row3").addClass("loadingImg");
+    $("#row4").addClass("loadingImg");
 }
